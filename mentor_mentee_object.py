2329d2c74ea2102748670aa07c0b3052cc1d4cd1
HuyGLe <hul104@ucsd.edu>
2019-04-07 10:45:10 -0700

    implemented matching algorithm

--- mentor_mentee_object.py
+++ mentor_mentee_object.py
@@ -3,26 +3,19 @@ import json
 class similarityObject:
     mentor = None
     mentee_list = []
-    similarity_dict = {}
     useless_words_array = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
     punctuation_array = [".", "!", "?", " ", ","]
 
-    def __init__(self, mentor, mentee_list):
+    def __init__(self, mentor, mentee):
         self.mentor = mentor
-        self.mentee_list = mentee_list
-        for mentee in mentee_list:
-            self.similarity_dict[mentee] = 0
+        self.mentee = mentee
 
-    def similarity_parse(self):
-        for mentee in self.mentee_list:
-            self.similarity_dict[mentee] = self.similarity_algorithm(self.mentor, mentee)
-
-    def similarity_algorithm(self, mentor, mentee):
+    def similarity_algorithm(self):
         mentor_bio_array = []
         mentee_bio_array = []
         similarity_index = 0
-        self.parsing(mentee.bio, mentee_bio_array)
-        self.parsing(mentor.bio, mentor_bio_array)
+        self.parsing(self.mentee.bio, mentee_bio_array)
+        self.parsing(self.mentor.bio, mentor_bio_array)
         for word in mentor_bio_array:
             if word in mentee_bio_array:
                 similarity_index = similarity_index + 1
@@ -96,12 +89,16 @@ def find_best_mentor():
 
         # call on the similarity algorithm that you (huy) wrote
         # use it on the mentee_obj variable and the temp_mentor
-        # it looks you're probably going to have to make a similarityObject
+        # it looks you're probably going to have to make a (similarityObject)
 
+        similar = similarityObject(temp_mentor, mentee_obj)
+        similarity_index = similar.similarity_algorithm()
 
         # if the similarity index is higher than highest_sim_index
         # then update that variable as well as best_mentor
-        best_mentor = temp_mentor
+        if similarity_index > highest_sim_index:
+            best_mentor = temp_mentor
+            highest_sim_index = similarity_index
 
     # return a string representation of the best mentor that can displayed on site
     return best_mentor.__repr__()
